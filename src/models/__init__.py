ba98543f524e2e25040d4a7a0acf8eee5f3f7dc3
nakamarusun <nakamarujc@gmail.com>
2021-03-02 22:02:28 +0700

    Completed REST get endpoint for workout levels

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -6,12 +6,10 @@ from firebase_admin import credentials, firestore, storage
 import google.cloud.storage
 
 # Use the application default credentials (By GOOGLE_APPLICATION_CREDENTIALS envvar)
+# Initialize account
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred, {
     'storageBucket': c.G_BUCKET_NAME
 })
 
-db = firestore.client()
 thumb_bucket = storage.bucket(c.G_BUCKET_NAME)
-
-bclient = google.cloud.storage.Client()
--- src/rest/workout.py
+++ src/rest/workout.py
@@ -1,8 +1,37 @@
 from flask_restful import Resource
-
+from firebase_admin import firestore
+from google.cloud import storage
+from models import thumb_bucket
 
 class WorkoutREST(Resource):
+
+    thumbnailsite = "https://storage.googleapis.com/{bucketname}/{thumbname}"
     
     @staticmethod
     def get(pack):
-        return {pack: 'world'}
+        # Make firestore client
+        fcl = firestore.client()
+
+        response = {"levels": "chapter_empty"}
+        
+        # Gets the document, and transforms it to python dictionary
+        level_doc_ref = fcl.collection('workouts').document(pack) # This is only the reference. Haven't pulled from the server
+        level_doc = level_doc_ref.get() # Here, data is actually pulled.
+
+        if level_doc.exists:
+            response = level_doc.to_dict()
+
+            # Create bucket storage client object
+            bcl = storage.Client()
+
+            # Replace every thumbnail with bucket link.
+            for level in response["levels"]:
+                file_name = level.get("thumbnail")
+                # Check whether the blob exist in the bucket
+
+                level['thumbnail'] = WorkoutREST.thumbnailsite.format(
+                    bucketname=thumb_bucket.name,
+                    thumbname=(file_name if storage.Blob(file_name, thumb_bucket).exists() else "default.png" if file_name else "default.png")
+                )
+            
+        return response

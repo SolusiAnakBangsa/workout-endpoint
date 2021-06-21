d6358e1552325357e406e55e1086426634432e14
nakamarusun <nakamarujc@gmail.com>
2021-06-21 20:27:53 +0700

    Added querying for server time

--- src/server.py
+++ src/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.blueprints import Blueprint
+import time
 import config as c
 import routes
 import models
@@ -8,6 +9,14 @@ app = Flask(__name__)
 
 # Set debug mode
 app.debug = c.DEBUG
+
+@app.route("/servertime")
+def server_time():
+    resp = {
+        "unixtime": time.time(),
+        "localtime": time.localtime()
+    }
+    return resp
 
 # Registers all the routes
 for blueprint in vars(routes).values():

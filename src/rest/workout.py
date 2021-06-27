4d91b03d78f59246b0e50949e7e4901930e94478
nakamarusun <nakamarujc@gmail.com>
2021-06-28 02:19:02 +0700

    Added POST method in workout to update all the workout data with password along with options preflight with CORS

--- src/rest/workout.py
+++ src/rest/workout.py
@@ -2,6 +2,11 @@ from flask_restful import Resource
 from firebase_admin import firestore
 from google.cloud import storage
 from models import thumb_bucket
+from flask import request
+
+from config import WORKOUT_PASS
+
+CORS_URL = "https://gameyourfit.com"
 
 class WorkoutREST(Resource):
 
@@ -31,4 +36,30 @@ class WorkoutREST(Resource):
                     thumbname=((file_name if storage.Blob(file_name, thumb_bucket).exists() else "default.png") if file_name else "default.png")
                 )
             
-        return response
+        return response, 200, {f"access-control-allow-origin": CORS_URL}
+
+    @staticmethod
+    def post(pack):
+        if WORKOUT_PASS == None: return
+
+        # Gets the json
+        data = request.get_json()
+
+        # Check password
+        if data["password"] == WORKOUT_PASS:
+            fcl = firestore.client()
+
+            # Commit data to the server
+            level_doc_ref = fcl.collection('workouts').document(pack)
+            level_doc_ref.set({
+                "levels": data["levels"]
+            })
+
+            return "", 200, {"access-control-allow-origin": CORS_URL}
+
+    @staticmethod
+    def options(pack):
+        return "", 200, {
+            "access-control-allow-origin": CORS_URL,
+            "Access-Control-Allow-Headers": "access-control-allow-origin, content-type"
+        }

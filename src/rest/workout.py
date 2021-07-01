571e1f9c153071321d7d22439f9940b084db3911
nakamarusun <nakamarujc@gmail.com>
2021-07-01 12:28:55 +0700

    Added URL check in the thumbnail workout get method, so that it doesnt send the default link if it is already a URL

--- src/rest/workout.py
+++ src/rest/workout.py
@@ -29,12 +29,14 @@ class WorkoutREST(Resource):
             # Replace every thumbnail with bucket link.
             for level in response["levels"]:
                 file_name = level.get("thumbnail")
-                # Check whether the blob exist in the bucket
 
-                level['thumbnail'] = WorkoutREST.thumbnailsite.format(
-                    bucketname=thumb_bucket.name,
-                    thumbname=((file_name if storage.Blob(file_name, thumb_bucket).exists() else "default.png") if file_name else "default.png")
-                )
+                # Check if the file is already a link first
+                if not (file_name.startswith("http://") or file_name.startswith("https://")):
+                    # Check whether the blob exist in the bucket
+                    level['thumbnail'] = WorkoutREST.thumbnailsite.format(
+                        bucketname=thumb_bucket.name,
+                        thumbname=((file_name if storage.Blob(file_name, thumb_bucket).exists() else "default.png") if file_name else "default.png")
+                    )
             
         return response, 200, {f"access-control-allow-origin": CORS_URL}
 
